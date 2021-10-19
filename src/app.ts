83da7882db8ca807164dbd07ba0c3c508e6661c5
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-10-19 20:27:54 -0300

    sokect io

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 import "dotenv/config"
-import express, { json } from "express"
+import express from "express"
+import http from "http"
+import cors from "cors"
+import { Server } from "socket.io"
 
 import { router } from "./routes"
 
 const app = express()
+app.use(cors())
+
+const serverHttp = http.createServer(app)
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+})
+
+io.on("connection", (socket) => {
+  console.log(`Usuário conectado no socket ${socket.id}`)
+})
 
 app.use(express.json())
 
@@ -21,4 +37,6 @@ app.get("/signin/callback", (request, response) => {
   return response.json(code)
 })
 
-app.listen(4000, () => console.log("Server running http://localhost:4000"))
+serverHttp.listen(4000, () =>
+  console.log("Server running http://localhost:4000")
+)
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,17 @@
 import { Router } from "express"
 import { AuthenticateUserController } from "./controller/AuthenticateUserController"
+import { CreateMessageController } from "./controller/CreateMessageController"
+
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated"
 
 const router = Router()
 
 router.post("/authenticate", new AuthenticateUserController().handle)
+
+router.post(
+  "/messages",
+  ensureAuthenticated,
+  new CreateMessageController().handle
+)
 
 export { router }
